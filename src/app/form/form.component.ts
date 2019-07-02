a0b060c5121b9aba51652d37fae0817ddd7b4bfd
ighrissi <ighrissi@cni.tn>
2019-07-02 10:19:33 +0200

    @viewChild exemple

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-form',
@@ -10,11 +10,16 @@ v1 : String;
 @Input() varchild;
 @Input() methchild;
 @Output() evenement=new EventEmitter<String>();
+@ViewChild('inputText', {static: true} )value :ElementRef
   constructor() { }
   
   ngOnInit() {
+    this.value.nativeElement.value='imen';
      }
   senddata(){
     this.evenement.emit(this.v1);
+   }
+   test(){
+     console.log(this.value.nativeElement.value);
    }
 }
